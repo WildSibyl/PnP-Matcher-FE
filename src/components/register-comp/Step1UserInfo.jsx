249de95af39c6fe80c8da747397a1d49e301a03e
Giada <giada.bellan@live.it>
2025-05-19 14:38:56 +0200

    amended register flow

--- src/components/register-comp/Step1UserInfo.jsx
+++ src/components/register-comp/Step1UserInfo.jsx
@@ -48,40 +48,6 @@ const Step1UserInfo = ({ form, onChange }) => (
       placeholder="Confirm Password"
       className="input-bordered"
     />
-    <div className="flex flex-row justify-between">
-      <label className="label">BIRTHDAY</label>
-      <p className="label-italic">We will only display your age</p>
-    </div>
-    <input
-      type="date"
-      name="birthday"
-      value={form.birthday}
-      onChange={onChange}
-      className="input-bordered"
-      max={new Date().toISOString().split("T")[0]}
-    />
-    <div className="flex flex-row justify-between">
-      <label className="label">ZIP CODE</label>
-      <p className="label-italic">Where is your castle placed?</p>
-    </div>
-    <input
-      name="zipCode"
-      value={form.zipCode}
-      onChange={onChange}
-      placeholder="Zip Code"
-      className="input-bordered"
-    />
-    <div className="flex flex-row justify-between">
-      <label className="label">COUNTRY</label>
-      <p className="label-italic">How is your land called?</p>
-    </div>
-    <input
-      name="country"
-      value={form.country}
-      onChange={onChange}
-      placeholder="Country"
-      className="input-bordered"
-    />
   </>
 );
 
