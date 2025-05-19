b84cc1889492b1870ad9d599562b91431ddeb061
LukasF7889 <lukas_fritsch@hotmail.de>
2025-05-19 12:20:00 +0200

    General Design Classes Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
 const Home = () => {
   return (
-    <div>
-      <h1>This is the H1</h1>
-      <h2>This is the H2</h2>
-      <h3>This is the H3</h3>
-      <p>This is an paragraph text</p>
-      <small>Those are smaller footnotes</small>
+    <div className="bg-gray-600">
+      <h1 className="text-pnp-white">This is the H1</h1>
+      <h2 className="text-pnp-white">This is the H2</h2>
+      <h3 className="text-pnp-white">This is the H3</h3>
+      <p className="text-pnp-white">This is an paragraph text</p>
+      <small className="text-pnp-white">Those are smaller footnotes</small>
+      <div className="w-[3vw] h-[3vh] bg-pnp-black"></div>
+      <div className="w-[3vw] h-[3vh] bg-pnp-green"></div>
+      <div className="w-[3vw] h-[3vh] bg-pnp-purple"></div>
+      <div className="w-[3vw] h-[3vh] bg-pnp-blue"></div>
+      <div className="w-[3vw] h-[3vh] bg-pnp-white"></div>
       <br />
       <button className="btn-primary-light">Primary LIGHT</button>
       <button className="btn-secondary-light">Secondary LIGHT</button>
-      <div className="bg-white p-8 m-4">
-        <button className="btn-primary-dark">Primary DARK</button>
-        <button className="btn-secondary-dark">Secondary DARK</button>
-      </div>
+      <button className="btn-primary-dark">Primary DARK</button>
+      <button className="btn-secondary-dark">Secondary DARK</button>
     </div>
   );
 };
