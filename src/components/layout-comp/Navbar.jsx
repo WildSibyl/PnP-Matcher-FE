e6e7145976f954130a4f74cd37b92f4fc24c3411
LukasF7889 <lukas_fritsch@hotmail.de>
2025-05-19 15:36:13 +0200

    Navbar scrolling implemented

--- src/components/layout-comp/Navbar.jsx
+++ src/components/layout-comp/Navbar.jsx
@@ -1,19 +1,64 @@
 import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import logo from "../../assets/logo.svg";
+import Chatbubblesvg from "../../assets/chatbubble.svg?react";
+import Closesvg from "../../assets/close.svg?react";
+import Burgermenuesvg from "../../assets/burgermenu.svg?react";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
   const { user, logOut } = useAuth();
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    let isScrolling = false;
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        isScrolling = true;
+      } else {
+        isScrolling = false;
+      }
+
+      setScrolled(isScrolling);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const setIconColor = () => {
+    if (scrolled) return "text-pnp-white";
+    return "text-pnp-white";
+  };
 
   return (
-    <div>
-      <a>NAVBAR</a>
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <Link to="/" className="btn btn-ghost text-xl">
-            PnPMatcher
-          </Link>
+    <div
+      className={`sticky top-0 w-full mt-0 pt-6 pb-3 px-6 z-15 transition-colors duration-300 ease-in-out ${
+        scrolled
+          ? "h-full w-full rounded-md bg-[rgba(112,36,219,0.1)] bg-clip-padding backdrop-filter backdrop-blur-sm pnp-shadow"
+          : "bg-transparent"
+      }`}
+    >
+      <div className="flex justify-between">
+        <Link to="/">
+          <img
+            src={logo}
+            className="cursor-pointer hover:scale-98 *:ease-in-out duration-200"
+            alt="pnpmatch logo"
+          ></img>
+        </Link>
+        <div className="flex gap-8">
+          <Chatbubblesvg className={`fill-current ${setIconColor()} `} />
+          <Burgermenuesvg className={`fill-current ${setIconColor()} `} />
         </div>
-        <div className="flex-none">
+      </div>
+    </div>
+  );
+};
+
+{
+  /* <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
               {user ? <NavLink to="/create">Create group</NavLink> : null}
@@ -33,10 +78,7 @@ const Navbar = () => {
               )}
             </li>
           </ul>
-        </div>
-      </div>
-    </div>
-  );
-};
+        </div> */
+}
 
 export default Navbar;
