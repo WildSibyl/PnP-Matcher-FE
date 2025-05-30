684105048e70c09af23e6fc4d5688b8dac95743c
LukasF7889 <lukas_fritsch@hotmail.de>
2025-05-30 12:01:51 +0200

    Switched Logo, implemented animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,16 @@ module.exports = {
           "80%": { transform: "scale(0.95) rotate(-2deg)" },
           "100%": { transform: "scale(1) rotate(0deg)" },
         },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
       },
       animation: {
         "tooltip-pop": "tooltip-pop 0.5s ease-out forwards",
+        "fade-in": "fadeIn 0.5s ease-in forwards",
       },
+
       fontFamily: {
         sans: ["Inter", "ui-sans-serif", "system-ui"],
       },
